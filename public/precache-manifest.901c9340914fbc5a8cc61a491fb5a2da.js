a62322b9042e86b5e5cdf60e813d829ce14683fe
Jose Miranda <jose.miranda@encodely.dev>
2020-05-04 18:41:05 -0600

    Add landing page

---
+++ public/precache-manifest.901c9340914fbc5a8cc61a491fb5a2da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f2e7dcaa1f0cafffaa9228d84b3361",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7ae7fc65225dfa256f97",
+    "url": "./static/css/main.ad2f70b9.chunk.css"
+  },
+  {
+    "revision": "3ac154d19e57b723c5f2",
+    "url": "./static/js/2.0cb959a3.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "./static/js/2.0cb959a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae7fc65225dfa256f97",
+    "url": "./static/js/main.9358f082.chunk.js"
+  },
+  {
+    "revision": "7bab8760c8a478c0d184",
+    "url": "./static/js/runtime-main.ee0e242b.js"
+  },
+  {
+    "revision": "4bff5d45104e2e6c59b1825b54864d58",
+    "url": "./static/media/bg4.4bff5d45.jpg"
+  }
+]);
--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-const functions = require("firebase-functions");
 const serverless = require("serverless-http");
 const express = require("express");
 const mongoose = require("mongoose");
@@ -35,4 +34,3 @@ app.listen(PORT, () => {
   console.log(`Server run on PORT ${PORT}`);
 });
 //module.exports.handler = serverless(app);
-exports.app = functions.https.onRequest(app);
